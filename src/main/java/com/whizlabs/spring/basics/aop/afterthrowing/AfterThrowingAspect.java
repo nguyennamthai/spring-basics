d1ffdf9331fddc6659133c414ca65a153b598e8f
Nguyen Nam Thai <namthainguyen@gmail.com>
2019-01-05 07:11:54 +0700

    Update 6.7

--- src/main/java/com/whizlabs/spring/basics/aop/afterthrowing/AfterThrowingAspect.java
+++ src/main/java/com/whizlabs/spring/basics/aop/afterthrowing/AfterThrowingAspect.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class AfterThrowingAspect {
     @AfterThrowing(pointcut = "execution(* throwException())", throwing = "exception")
     private void printMethodResult(JoinPoint joinPoint, Exception exception) {
-        System.out.println("An exception was thrown by the method " + joinPoint.getSignature().getName() +
-                "\nMessage: " + exception.getMessage());
+        System.out.println("An exception was thrown by the method " + joinPoint.getSignature().getName());
+        System.out.println("Message: " + exception.getMessage());
     }
 }
